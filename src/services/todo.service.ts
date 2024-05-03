97a595f72292450b5b1d5f56e06aa04b928fab47
Viktor Shaparenko <vkshadyafk@gmail.com>
2024-05-03 21:05:00 +0300

    add pagination

--- src/services/todo.service.ts
+++ src/services/todo.service.ts
@@ -1,15 +1,35 @@
 import { TodoModel } from '../models/Todo';
+import { Todo } from '../types/interface';
+import { calculatePages } from '../utilities/utilities';
 
-interface Todo {
-  title: string;
-  body: string;
-  status: string;
-  boardId: string;
-}
+export const getAllFromCurrentBoard = async (
+  boardId: string,
+  page?: string,
+) => {
+  const todosPerPage = 9;
+  const currentPage = page || '1';
 
-export const getAll = async () => {
-  const todos = await TodoModel.find();
-  return todos;
+  const todos = await TodoModel.find(
+    { boardId },
+    'id title body boardId status',
+  )
+    .skip(todosPerPage * Number(currentPage) - todosPerPage)
+    .limit(todosPerPage)
+    .exec();
+
+  const count = await TodoModel.countDocuments({ boardId });
+
+  const { previousPage, nextPage } = calculatePages(
+    currentPage,
+    count,
+    todosPerPage,
+  );
+  return {
+    count,
+    todos,
+    previousPage,
+    nextPage,
+  };
 };
 
 export const createNewTodo = async (todoForCreating: Todo) => {
@@ -33,6 +53,7 @@ export const updateTodo = async (todoForUpdate: Todo, todoId: string) => {
 
   return updatedTodo;
 };
+
 
 export const deleteTodo = async (todoId: string) => {
   await TodoModel.findByIdAndDelete(todoId);
---
+++ src/types/interface.ts
@@ -0,0 +1,8 @@
+import { Status } from './types';
+
+export interface Todo {
+  title: string;
+  body: string;
+  status: string;
+  boardId: Status;
+}
